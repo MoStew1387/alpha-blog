a754f6f87eab870366ef2f4a9438c96cf3464c12
MoStew1387 <m.carrick.190815@gmail.com>
2018-02-07 19:34:21 +0000

    Fix edit, add flash and error messages, create edit being shown

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,17 +4,31 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def create
     @article = Article.new(article_params)
     # @article.save
     
     if @article.save
-      flash[:notice] = "Article was sucessfully created."
+      flash[:notice] = "Article was successfully created."
       redirect_to article_path(@article)
     else
       render 'new'
     end
     # redirect_to article_path(article)
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated."
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
   
   def show
